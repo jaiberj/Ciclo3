a78cdd41c25cfb75764afea3ed57f44d27dfb6aa
jaiberj <jaiberj1@gmail.com>
2022-10-11 12:20:48 -0500

    commit 14

--- src/main/java/com/jaiberuribe/ciclo3/controllers/OrtopedicController.java
+++ src/main/java/com/jaiberuribe/ciclo3/controllers/OrtopedicController.java
@@ -7,22 +7,12 @@ import com.jaiberuribe.ciclo3.model.Ortopedic;
 import com.jaiberuribe.ciclo3.service.OrtopedicService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
 @RequestMapping("/api/Ortopedic")
-@CrossOrigin(origins = "*")
-
+@CrossOrigin(origins = "*", methods = { RequestMethod.PUT, RequestMethod.DELETE, RequestMethod.GET, RequestMethod.POST })
 public class OrtopedicController {
     @Autowired
     private OrtopedicService ortopedicService;
